6c3ff2aff8ca6114ac9fcb6b1f394f293d4134d7
Alessandro Schena <Alessandro.Schena@constructor.org>
2024-08-08 15:21:42 +0200

    Add FileProcessor.

--- src/lib/eiffel.rs
+++ src/lib/eiffel.rs
@@ -2,12 +2,61 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use std::path::{Path, PathBuf};
-use tree_sitter::{InputEdit, Language, Parser, Point, Tree};
+use tree_sitter::{InputEdit, Language, Parser, Point, Tree, TreeCursor};
 
 struct Workspace<'a> {
     classes: Vec<Class<'a>>,
-    parsed_files: HashMap<PathBuf, Tree>,
+    files: HashMap<PathBuf, Tree>,
     parser: Parser,
+}
+
+struct FileProcessor {
+    tree: Tree,
+    path: PathBuf,
+    src: Vec<u8>,
+}
+impl FileProcessor {
+    fn new(parser: &mut Parser, path: PathBuf) -> FileProcessor {
+        let src = std::fs::read(&path).expect("Failed to read file.");
+        let tree = parser.parse(&src, None).unwrap();
+        FileProcessor { tree, path, src }
+    }
+    fn class(&mut self) -> Option<Class> {
+        let mut cursor = self.tree.walk();
+        self.class_helper(&mut cursor)
+    }
+    fn class_helper<'a>(&self, c: &mut TreeCursor) -> Option<Class<'a>> {
+        if c.node().kind() == "class_name" {
+            let name = match String::from_utf8(self.src[c.node().byte_range()].to_vec()) {
+                Ok(v) => v,
+                Err(e) => panic!("invalid UTF-8 sequence {}", e),
+            };
+            let source_file = self.path.clone();
+            let features = Vec::new();
+            let descendants = Vec::new();
+            let ancestors = Vec::new();
+            Some(Class {
+                name,
+                source_file,
+                features,
+                descendants,
+                ancestors,
+            })
+        } else {
+            if c.goto_next_sibling() {
+                self.class_helper(c)
+            } else {
+                if c.goto_first_child() {
+                    self.class_helper(c)
+                } else {
+                    None
+                }
+            }
+        }
+    }
+    fn features(&mut self) -> Vec<Feature> {
+        todo!()
+    }
 }
 
 impl Workspace<'_> {
@@ -19,17 +68,39 @@ impl Workspace<'_> {
 
         Workspace {
             classes: Vec::new(),
-            parsed_files: HashMap::new(),
+            files: HashMap::new(),
             parser,
         }
     }
-    fn add_file(&mut self, filepath: PathBuf) {
-        let mut f = File::open(&filepath).expect("Failed to open file.");
-        let mut src = String::new();
-        f.read_to_string(&mut src).expect("Failed to read file.");
-        let tree = self.parser.parse(src, None).unwrap();
-        self.parsed_files.insert(filepath, tree);
+    fn add_file(&mut self, filepath: PathBuf) {}
+    fn init_classes(&mut self) {}
+}
+
+/// Registers the class of a file. There must be a signle class per file.
+fn init_class<'a>(cursor: &mut TreeCursor) -> Option<Class<'a>> {
+    todo!()
+}
+
+fn init_features<'a>(cursor: &mut TreeCursor, acc: &mut Vec<Feature<'a>>) -> Vec<Feature<'a>> {
+    debug_assert!(cursor.node().kind() == "feature_declaration");
+    let declaration_node = cursor.node();
+    if cursor.goto_first_child() {
+        debug_assert!(cursor.node().kind() == "new_feature");
+        acc.push(Feature {
+            name: todo!(),
+            visibility: FeatureVisibility::None,
+        });
+        while !cursor.goto_next_sibling() {
+            if cursor.node().kind() == "new_feature" {
+                todo!()
+            }
+        }
+        cursor.reset(declaration_node);
+        if cursor.goto_next_sibling() {
+            init_features(cursor, acc);
+        }
     }
+    todo!();
 }
 
 #[derive(Debug)]
@@ -48,7 +119,7 @@ struct Feature<'a> {
 #[derive(Debug)]
 struct Class<'a> {
     name: String,
-    source_file: &'a Path,
+    source_file: PathBuf,
     features: Vec<Feature<'a>>,
     descendants: Vec<&'a Class<'a>>,
     ancestors: Vec<&'a Class<'a>>,
