0a8941899b13ae51de58f6ed3958a9c28a209305
Alesandro Schena <alessandro.schena.97@protonmail.com>
2024-08-21 22:07:30 +0200

    `from_processed_file` -> `from_tree_and_src`

--- src/lib/code_entities.rs
+++ src/lib/code_entities.rs
@@ -1,6 +1,7 @@
 use super::processed_file::ProcessedFile;
 use super::tree_sitter::WidthFirstTraversal;
 use std::path::PathBuf;
+use tree_sitter::Tree;
 
 #[derive(Debug, PartialEq, Eq)]
 pub(super) enum FeatureVisibility<'a> {
@@ -49,12 +50,12 @@ pub(crate) struct CodeEntities<'a> {
 }
 
 impl CodeEntities<'_> {
-    pub(crate) fn from_processed_file(file: &ProcessedFile) -> CodeEntities<'_> {
-        let cursor = file.tree.walk();
+    pub(crate) fn from_tree_and_src<'a, 'b>(tree: &'a Tree, src: &'a Vec<u8>) -> CodeEntities<'b> {
+        let cursor = tree.walk();
         let mut traversal = WidthFirstTraversal::new(cursor);
 
         let class = match String::from_utf8(
-            file.src[traversal
+            src[traversal
                 .find(|x| x.kind() == "class_name")
                 .expect("class_name")
                 .byte_range()]
@@ -67,7 +68,7 @@ impl CodeEntities<'_> {
         let features = traversal
             .filter(|x| x.kind() == "extended_feature_name")
             .map(
-                |node| match String::from_utf8(file.src[node.byte_range()].to_vec()) {
+                |node| match String::from_utf8(src[node.byte_range()].to_vec()) {
                     Ok(v) => Feature::from_name(v),
                     Err(e) => panic!("invalid UTF-8 sequence {}", e),
                 },
@@ -81,16 +82,39 @@ impl CodeEntities<'_> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::File;
+    use std::io::prelude::*;
 
-    const BASIC_CLASS_PATH: &str = "/tmp/basic_class.e";
-    const BASIC_CLASS: &str = "
+    #[test]
+    fn process_base_class() -> std::io::Result<()> {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(tree_sitter_eiffel::language())
+            .expect("Error loading Eiffel grammar");
+
+        let src = "
 class A
 note
 end
     ";
+        let tree = parser.parse(src, None).unwrap();
 
-    const ANNOTATED_CLASS_PATH: &str = "/tmp/annotated_class.e";
-    const ANNOTATED_CLASS: &str = "
+        let CodeEntities { class, features: _ } =
+            CodeEntities::from_tree_and_src(&tree, &src.into());
+
+        assert_eq!(class, Class::from_name("A".to_string()));
+
+        Ok(())
+    }
+
+    #[test]
+    fn process_annotated_class() -> std::io::Result<()> {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(tree_sitter_eiffel::language())
+            .expect("Error loading Eiffel grammar");
+
+        let src = "
 note
   demo_note: True
   multi_note: True, False
@@ -100,61 +124,10 @@ invariant
     note_after_invariant: True
 end
     ";
+        let tree = parser.parse(src, None).unwrap();
 
-    const ATTRIBUTE_PATH: &str = "/tmp/class_with_feature_path.e";
-    const ATTRIBUTE: &str = "
-class A
-feature
-  x: INTEGER
-end
-";
-
-    const PROCEDURE_PATH: &str = "/tmp/class_with_feature_path.e";
-    const PROCEDURE: &str = "
-class A feature
-  f(x, y: INTEGER; z: BOOLEAN)
-    do
-    end
-end
-";
-
-    use std::fs::File;
-    use std::io::prelude::*;
-
-    #[test]
-    fn process_base_class() -> std::io::Result<()> {
-        let path: PathBuf = PathBuf::from(BASIC_CLASS_PATH);
-        let mut file = File::create(&path)?;
-        file.write_all(BASIC_CLASS.as_bytes())?;
-
-        let mut parser = tree_sitter::Parser::new();
-        parser
-            .set_language(tree_sitter_eiffel::language())
-            .expect("Error loading Eiffel grammar");
-
-        let file = ProcessedFile::new(&mut parser, path.clone());
-
-        let CodeEntities { class, features: _ } = CodeEntities::from_processed_file(&file);
-
-        assert_eq!(class, Class::from_name("A".to_string()));
-
-        Ok(())
-    }
-
-    #[test]
-    fn process_annotated_class() -> std::io::Result<()> {
-        let path: PathBuf = PathBuf::from(ANNOTATED_CLASS_PATH);
-        let mut file = File::create(&path)?;
-        file.write_all(ANNOTATED_CLASS.as_bytes())?;
-
-        let mut parser = tree_sitter::Parser::new();
-        parser
-            .set_language(tree_sitter_eiffel::language())
-            .expect("Error loading Eiffel grammar");
-
-        let file = ProcessedFile::new(&mut parser, path.clone());
-
-        let CodeEntities { class, features: _ } = CodeEntities::from_processed_file(&file);
+        let CodeEntities { class, features: _ } =
+            CodeEntities::from_tree_and_src(&tree, &src.into());
 
         assert_eq!(class, Class::from_name("DEMO_CLASS".to_string()));
 
@@ -163,18 +136,20 @@ end
 
     #[test]
     fn process_procedure() -> std::io::Result<()> {
-        let path: PathBuf = PathBuf::from(PROCEDURE_PATH);
-        let mut file = File::create(&path)?;
-        file.write_all(PROCEDURE.as_bytes())?;
-
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(tree_sitter_eiffel::language())
             .expect("Error loading Eiffel grammar");
 
-        let file = ProcessedFile::new(&mut parser, path.clone());
-
-        let CodeEntities { class, features } = CodeEntities::from_processed_file(&file);
+        let src = "
+class A feature
+  f(x, y: INTEGER; z: BOOLEAN)
+    do
+    end
+end
+";
+        let tree = parser.parse(src, None).unwrap();
+        let CodeEntities { class, features } = CodeEntities::from_tree_and_src(&tree, &src.into());
 
         assert_eq!(class, Class::from_name("A".to_string()));
         assert_eq!(features, vec![Feature::from_name("f".to_string())]);
@@ -184,18 +159,20 @@ end
 
     #[test]
     fn process_attribute() -> std::io::Result<()> {
-        let path: PathBuf = PathBuf::from(ATTRIBUTE_PATH);
-        let mut file = File::create(&path)?;
-        file.write_all(ATTRIBUTE.as_bytes())?;
-
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(tree_sitter_eiffel::language())
             .expect("Error loading Eiffel grammar");
 
-        let file = ProcessedFile::new(&mut parser, path.clone());
+        let src = "
+class A
+feature
+    x: INTEGER
+end
+";
+        let tree = parser.parse(src, None).unwrap();
 
-        let CodeEntities { class, features } = CodeEntities::from_processed_file(&file);
+        let CodeEntities { class, features } = CodeEntities::from_tree_and_src(&tree, &src.into());
 
         assert_eq!(class, Class::from_name("A".to_string()));
         assert_eq!(features, vec![Feature::from_name("x".to_string())]);
