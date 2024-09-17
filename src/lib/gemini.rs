f3be0d3ea60b4127cf0eeec72d423fd46904dbb1
Alessandro Schena <Alessandro.Schena@constructor.org>
2024-09-17 19:35:46 +0200

    feat: Add gemini API types and their (de)?serialization.

--- src/lib/gemini.rs
+++ src/lib/gemini.rs
@@ -1,45 +1,238 @@
 use reqwest;
+use serde::{Deserialize, Serialize};
+use std::env;
 use std::fmt::format;
 use std::fmt::Display;
+use std::future;
+use std::str::FromStr;
 /// Required. The content of the current conversation with the model.
 /// For single-turn queries, this is a single instance.
 /// For multi-turn queries like chat, this is a repeated field that contains the conversation history and the latest request.
+#[derive(Serialize, Debug)]
 struct Contents {
-    parts: Vec<Parts>,
+    parts: Vec<Part>,
 }
-struct Parts(Vec<String>);
+impl FromStr for Contents {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = vec![s.parse().expect("Parse string into LSP-type `Part`")];
+        Ok(Self { parts })
+    }
+}
+impl From<String> for Contents {
+    fn from(value: String) -> Self {
+        let parts = vec![value.into()];
+        Self { parts }
+    }
+}
+#[derive(Deserialize, Serialize, Debug)]
+struct Part {
+    text: String,
+}
+impl FromStr for Part {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Part {
+            text: s.to_string(),
+        })
+    }
+}
+impl From<String> for Part {
+    fn from(value: String) -> Self {
+        let text = value;
+        Self { text }
+    }
+}
 
 /// Optional. A list of Tools the Model may use to generate the next response.
 /// A Tool is a piece of code that enables the system to interact with external systems to perform an action, or set of actions, outside of knowledge and scope of the Model. Supported Tools are Function and codeExecution. Refer to the Function calling and the Code execution guides to learn more.
+#[derive(Serialize, Debug)]
 struct Tools;
 /// Optional. Tool configuration for any Tool specified in the request. Refer to the Function calling guide for a usage example.
+#[derive(Serialize, Debug)]
 struct ToolConfig;
 ///Optional. A list of unique SafetySetting instances for blocking unsafe content.
 ///This will be enforced on the GenerateContentRequest.contents and GenerateContentResponse.candidates. There should not be more than one setting for each SafetyCategory type. The API will block any contents and responses that fail to meet the thresholds set by these settings. This list overrides the default settings for each SafetyCategory specified in the safetySettings. If there is no SafetySetting for a given SafetyCategory provided in the list, the API will use the default safety setting for that category. Harm categories HARM_CATEGORY_HATE_SPEECH, HARM_CATEGORY_SEXUALLY_EXPLICIT, HARM_CATEGORY_DANGEROUS_CONTENT, HARM_CATEGORY_HARASSMENT are supported. Refer to the guide for detailed information on available safety settings. Also refer to the Safety guidance to learn how to incorporate safety considerations in your AI applications.
+#[derive(Serialize, Debug)]
 struct SafetySetting;
 /// Optional. Developer set system instruction(s). Currently, text only.
-struct SystemInstruction;
+#[derive(Serialize, Debug)]
+struct SystemInstruction {
+    content: Content,
+}
+impl FromStr for SystemInstruction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let content = s.parse().expect("Parse string into LSP type `Content`");
+        Ok(Self { content })
+    }
+}
 /// Optional. Configuration options for model generation and outputs.
+#[derive(Serialize, Debug)]
 struct GenerationConfig;
 /// Optional. The name of the content cached to use as context to serve the prediction. Format: cachedContents/{cachedContent}
+#[derive(Serialize, Debug)]
 struct CachedContent;
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct Request {
     contents: Contents,
+    #[serde(skip_serializing_if = "Option::is_none")]
     tools: Option<Tools>,
-    toolConfig: Option<ToolConfig>,
-    safetySettings: Option<Vec<SafetySetting>>,
-    systemInstruction: Option<SystemInstruction>,
-    generationConfig: Option<GenerationConfig>,
-    cachedContent: Option<CachedContent>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    tool_config: Option<ToolConfig>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    safety_settings: Option<Vec<SafetySetting>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    system_instruction: Option<SystemInstruction>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    generation_config: Option<GenerationConfig>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    cached_content: Option<CachedContent>,
+}
+impl Request {
+    async fn process(&self, config: &Config) -> Response {
+        let web_client = reqwest::Client::new();
+        let json_req = web_client.post(config.end_point().clone()).json(&self);
+        eprintln!("{:?}", json_req);
+        match json_req.send().await {
+            Ok(res) => {
+                let response = res
+                    .json::<Response>()
+                    .await
+                    .expect("Decode response from Gemini");
+                response
+            }
+            Err(_) => {
+                panic!("Response of Gemini")
+            }
+        }
+    }
+    fn set_system_instruction(&mut self, system_instruction: SystemInstruction) {
+        self.system_instruction = Some(system_instruction);
+    }
+}
+impl FromStr for Request {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let contents = s.parse().expect("Parse string to LSP-type contents");
+        Ok(Self {
+            contents,
+            tools: None,
+            tool_config: None,
+            safety_settings: None,
+            system_instruction: None,
+            generation_config: None,
+            cached_content: None,
+        })
+    }
+}
+impl From<String> for Request {
+    fn from(value: String) -> Self {
+        let contents = value.into();
+        Self {
+            contents,
+            tools: None,
+            tool_config: None,
+            safety_settings: None,
+            system_instruction: None,
+            generation_config: None,
+            cached_content: None,
+        }
+    }
 }
 /// Candidate responses from the model.
+#[derive(Deserialize, Debug)]
 struct Candidates(Vec<Candidate>);
-struct Candidate(String);
+#[derive(Deserialize, Debug)]
+struct Candidate {
+    content: Content,
+    #[serde(skip)]
+    finish_reason: Option<FinishReason>,
+    #[serde(skip)]
+    safety_ratings: SafetyRatings,
+    #[serde(skip)]
+    citation_metadata: CitationMetadata,
+    #[serde(skip)]
+    token_count: TokenCount,
+    #[serde(skip)]
+    grounding_attributions: GroundingAttributions,
+    #[serde(skip)]
+    index: Index,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Content {
+    parts: Vec<Part>,
+    role: Option<Role>,
+}
+impl Default for Content {
+    fn default() -> Self {
+        let parts = Vec::new();
+        let role = Some(Role::Model);
+        Self { parts, role }
+    }
+}
+impl FromStr for Content {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = vec![s.parse().expect("Parse string into LSP type `Part`")];
+        Ok(Self {
+            parts,
+            ..Default::default()
+        })
+    }
+}
+#[derive(Serialize, Deserialize, Debug)]
+enum Role {
+    #[serde(rename(deserialize = "user"))]
+    User,
+    #[serde(rename(deserialize = "model"))]
+    Model,
+}
+/// Optional. Output only. The reason why the model stopped generating tokens. If empty, the model has not stopped generating tokens.
+#[derive(Deserialize, Debug)]
+enum FinishReason {}
+/// List of ratings for the safety of a response candidate. There is at most one rating per category.
+#[derive(Deserialize, Debug, Default)]
+struct SafetyRatings {}
+/// Output only. Citation information for model-generated candidate. This field may be populated with recitation information for any text included in the content. These are passages that are "recited" from copyrighted material in the foundational LLM's training data.
+#[derive(Deserialize, Debug, Default)]
+struct CitationMetadata {}
+/// Output only. Token count for this candidate.
+#[derive(Deserialize, Debug, Default)]
+struct TokenCount {}
+/// Output only. Attribution information for sources that contributed to a grounded answer. This field is populated for GenerateAnswer calls.
+#[derive(Deserialize, Debug, Default)]
+struct GroundingAttributions {}
+/// Output only. Index of the candidate in the list of response candidates.
+#[derive(Deserialize, Debug, Default)]
+struct Index {}
+
 /// Returns the prompt's feedback related to the content filters.
-struct PromptFeedback;
+#[derive(Deserialize, Debug, Default)]
+struct PromptFeedback {
+    block_reason: Option<BlockReason>,
+    safety_ratings: SafetyRatings,
+}
+#[derive(Deserialize, Debug)]
+enum BlockReason {}
 /// Output only. Metadata on the generation requests' token usage.
+#[derive(Deserialize, Debug, Default)]
 struct UsageMetadata;
-struct Response {}
+#[derive(Deserialize, Debug)]
+struct Response {
+    candidates: Candidates,
+    #[serde(skip_deserializing)]
+    prompt_feedback: PromptFeedback,
+    #[serde(skip_deserializing)]
+    usage_metadata: UsageMetadata,
+}
+#[derive(Debug, Clone)]
 enum Mode {
     Generate,
     Stream,
@@ -52,6 +245,7 @@ impl Display for Mode {
         }
     }
 }
+#[derive(Debug, Clone)]
 enum Model {
     Flash,
     Pro,
@@ -64,53 +258,68 @@ impl Display for Model {
         }
     }
 }
+#[derive(Debug)]
 struct Config {
-    end_point: reqwest::Url,
+    token: String,
     mode: Mode,
     model: Model,
 }
 impl Config {
-    fn initialize(model: Model, mode: Mode) -> Config {
-        let string_end_point = format!("https://generativelanguage.googleapis.com/v1beta/models/{model}:{mode}?key=$GOOGLE_API_KEY");
-        match string_end_point.as_str().try_into() {
-            Ok(end_point) => Config {
-                end_point,
-                mode,
-                model,
-            },
-            Err(e) => {
-                panic!()
-            }
-        }
+    fn new(model: Model, mode: Mode, env_var_token: String) -> Config {
+        let token = env::var(env_var_token).expect("Environment variable containing gemini token.");
+        Config { token, mode, model }
+    }
+    fn end_point(&self) -> reqwest::Url {
+        let model = self.model.clone();
+        let mode = self.mode.clone();
+        let end_point =
+            format!("https://generativelanguage.googleapis.com/v1beta/models/{model}:{mode}?key=")
+                + self.token.as_str();
+        end_point
+            .as_str()
+            .try_into()
+            .expect("Initialize config Gemini")
     }
 }
-/// Interface of Gemini
-struct Gemini {
-    response: Option<Response>,
-    request: Option<Request>,
-    config: Config,
-}
-impl Gemini {
-    /// Set request.
-    fn request(&mut self, req: Request) {
-        self.request = Some(req);
-    }
-    /// Get response.
-    fn response(&self) -> &Option<Response> {
-        &self.response
-    }
-}
-impl Default for Gemini {
+impl Default for Config {
     fn default() -> Self {
-        let response = None;
-        let request = None;
-        let model = Model::Flash;
         let mode = Mode::Generate;
-        let config = Config::initialize(model, mode);
-        Self {
-            response,
-            request,
-            config,
-        }
+        let model = Model::Flash;
+        let env_var_token = "GOOGLE_API_KEY".to_string();
+        Self::new(model, mode, env_var_token)
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn serialize_request() {
+        let req =
+            Request::from("Write a story about turles from the prospective of a frog.".to_string());
+        eprintln!("{:?}", serde_json::to_string(&req));
+    }
+    #[tokio::test]
+    async fn text_response() {
+        let req =
+            Request::from("Write a story about turles from the prospective of a frog.".to_string());
+        let config = Config::default();
+        let web_client = reqwest::Client::new();
+        let json_req = web_client.post(config.end_point().clone()).json(&req);
+        eprintln!("{:?}", json_req);
+        let res = match json_req.send().await {
+            Ok(res) => res.text().await.expect("Decode response from Gemini"),
+            Err(_) => {
+                panic!("Response of Gemini")
+            }
+        };
+        eprintln!("{:?}", res);
+    }
+    #[tokio::test]
+    async fn json_response() {
+        let req =
+            Request::from("Write a story about turles from the prospective of a frog.".to_string());
+        let config = Config::default();
+        let res = req.process(&config).await;
+        eprintln!("{:?}", res);
     }
 }
