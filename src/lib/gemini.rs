cb7b34db6ee9539bdab6eff315b0f356b2ffda61
Alessandro Schena <Alessandro.Schena@constructor.org>
2024-09-19 18:55:56 +0200

    feat(gemini): Begin to specialize the request configuration to output JSONs.

--- src/lib/gemini.rs
+++ src/lib/gemini.rs
@@ -1,5 +1,6 @@
 use reqwest;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::env;
 use std::fmt::format;
 use std::fmt::Display;
@@ -72,7 +73,162 @@ impl FromStr for SystemInstruction {
 }
 /// Optional. Configuration options for model generation and outputs.
 #[derive(Serialize, Debug)]
-struct GenerationConfig;
+struct GenerationConfig {
+    stop_sequences: Option<StopSequence>,
+    response_mime_type: Option<ResponseMimeType>,
+    response_schema: Option<ResponseSchema>,
+    /// Number of generated responses to return.
+    /// Currently, this value can only be set to 1. If unset, this will default to 1.
+    candidate_count: Option<i32>,
+    /// The maximum number of tokens to include in a response candidate.
+    /// Note: The default value varies by model, see the Model.output_token_limit attribute of the Model returned from the getModel function.
+    max_output_token: Option<i32>,
+    /// Controls the randomness of the output.
+    /// Note: The default value varies by model, see the Model.temperature attribute of the Model returned from the getModel function.
+    /// Values can range from [0.0, 2.0].
+    temperature: Option<i32>,
+    // The maximum cumulative probability of tokens to consider when sampling.
+    // The model uses combined Top-k and Top-p (nucleus) sampling.
+    // Tokens are sorted based on their assigned probabilities so that only the most likely tokens are considered. Top-k sampling directly limits the maximum number of tokens to consider, while Nucleus sampling limits the number of tokens based on the cumulative probability.
+    // Note: The default value varies by Model and is specified by theModel.top_p attribute returned from the getModel function. An empty topK attribute indicates that the model doesn't apply top-k sampling and doesn't allow setting topK on requests.
+    top_p: Option<i32>,
+    /// The maximum number of tokens to consider when sampling.
+    /// Gemini models use Top-p (nucleus) sampling or a combination of Top-k and nucleus sampling. Top-k sampling considers the set of topK most probable tokens. Models running with nucleus sampling don't allow topK setting.
+    /// Note: The default value varies by Model and is specified by theModel.top_p attribute returned from the getModel function. An empty topK attribute indicates that the model doesn't apply top-k sampling and doesn't allow setting topK on requests.
+    top_k: Option<i32>,
+}
+/// Optional. The set of character sequences (up to 5) that will stop output generation. If specified, the API will stop at the first appearance of a stop_sequence. The stop sequence will not be included as part of the response.
+#[derive(Serialize, Debug)]
+struct StopSequence(Vec<String>);
+
+/// Optional. MIME type of the generated candidate text. Supported MIME types are: text/plain: (default) Text output. application/json: JSON response in the response candidates. Refer to the docs for a list of all supported text MIME types.
+#[derive(Serialize, Debug)]
+enum ResponseMimeType {
+    #[serde(rename(serialize = "application/json"))]
+    Json,
+    #[serde(rename(serialize = "text/plain"))]
+    Text,
+}
+
+/// Optional. Output schema of the generated candidate text. Schemas must be a subset of the OpenAPI schema and can be objects, primitives or arrays.
+/// If set, a compatible responseMimeType must also be set. Compatible MIME types: application/json: Schema for JSON response. Refer to the JSON text generation guide for more details.
+#[derive(Serialize, Debug)]
+struct ResponseSchema {
+    #[serde(rename(serialize = "type"))]
+    schema_type: SchemaType,
+    /// The format of the data
+    /// This is used only for primitive datatypes
+    /// Supported formats: for NUMBER type: float, double for INTEGER type: int32, int64 for STRING type: enum
+    format: Option<String>,
+    /// A brief description of the parameter
+    /// This could contain examples of use
+    /// Parameter description may be formatted as Markdown
+    description: Option<String>,
+    /// Indicates if the value may be null
+    nullable: Option<bool>,
+    /// Possible values of the element of Type
+    /// STRING with enum format
+    /// For example we can define an Enum Direction as : {type:STRING, format:enum, enum:["EAST", NORTH", "SOUTH", "WEST"]}
+    #[serde(rename(serialize = "enum"))]
+    possibilities: Option<String>,
+    /// Maximum number of the elements for Type.ARRAY.
+    #[serde(rename(serialize = "maxItems"))]
+    max_items: Option<String>,
+    ///Properties of Type.OBJECT.
+    /// An object containing a list of "key": value pairs. Example: { "name": "wrench", "mass": "1.3kg", "count": "3" }.
+    properties: Option<HashMap<String, ResponseSchema>>,
+    /// Optional. Required properties of Type.OBJECT.
+    required: Option<Vec<String>>,
+    /// Schema of the elements of Type.ARRAY.
+    items: Option<Box<ResponseSchema>>,
+}
+#[derive(Serialize, Debug)]
+enum SchemaType {
+    #[serde(rename(serialize = "STRING"))]
+    String,
+    #[serde(rename(serialize = "NUMBER"))]
+    Number,
+    #[serde(rename(serialize = "INTEGER"))]
+    Integer,
+    #[serde(rename(serialize = "BOOLEAN"))]
+    Boolean,
+    #[serde(rename(serialize = "ARRAY"))]
+    Array,
+    #[serde(rename(serialize = "OBJECT"))]
+    Object,
+}
+impl ResponseSchema {
+    pub fn contracts() -> ResponseSchema {
+        let pre = "preconditions".to_string();
+        let post = "postconditions".to_string();
+        let properties = Some(HashMap::from([
+            (pre.clone(), ResponseSchema::preconditions()),
+            (post.clone(), ResponseSchema::postconditions()),
+        ]));
+        let required = Some(vec![pre, post]);
+        ResponseSchema {
+            schema_type: SchemaType::Object,
+            format: None,
+            description: None,
+            nullable: Some(false),
+            possibilities: None,
+            max_items: None,
+            properties,
+            required,
+            items: None,
+        }
+    }
+    fn contract() -> ResponseSchema {
+        let items = Some(Box::from(ResponseSchema::contract_clause()));
+        ResponseSchema {
+            schema_type: SchemaType::Array,
+            format: None,
+            description: None,
+            nullable: Some(true),
+            possibilities: None,
+            max_items: None,
+            properties: None,
+            required: None,
+            items,
+        }
+    }
+    fn preconditions() -> ResponseSchema {
+        let mut schema = ResponseSchema::contract();
+        let items = Some(Box::from(ResponseSchema::precondition_clause()));
+        schema.description = todo!();
+        schema.items = items;
+        schema
+    }
+    fn postconditions() -> ResponseSchema {
+        let mut schema = ResponseSchema::contract();
+        schema.description = todo!();
+        schema
+    }
+    fn contract_clause() -> ResponseSchema {
+        ResponseSchema {
+            schema_type: SchemaType::String,
+            format: None,
+            description: None,
+            nullable: Some(false),
+            possibilities: None,
+            max_items: None,
+            properties: None,
+            required: None,
+            items: None,
+        }
+    }
+    fn precondition_clause() -> ResponseSchema {
+        let pre = ResponseSchema::contract_clause();
+        pre.description = todo!();
+        pre
+    }
+    fn postcondition_clause() -> ResponseSchema {
+        let post = ResponseSchema::contract_clause();
+        post.description = todo!();
+        post
+    }
+}
+
 /// Optional. The name of the content cached to use as context to serve the prediction. Format: cachedContents/{cachedContent}
 #[derive(Serialize, Debug)]
 struct CachedContent;
@@ -114,6 +270,7 @@ impl Request {
     fn set_system_instruction(&mut self, system_instruction: SystemInstruction) {
         self.system_instruction = Some(system_instruction);
     }
+    fn set_json_response() {}
 }
 impl FromStr for Request {
     type Err = ();
