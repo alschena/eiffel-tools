ba1ad76cca3872c0dd520243d11f07507fbb2fdf
Alesandro Schena <alessandro.schena.97@protonmail.com>
2024-10-06 16:50:12 +0200

    feat: Do not store all source code in a processed file

--- src/lib/processed_file.rs
+++ src/lib/processed_file.rs
@@ -2,20 +2,20 @@ use super::code_entities::Class;
 use super::code_entities::Feature;
 use super::code_entities::Range;
 use super::tree_sitter::ExtractedFrom;
+use anyhow::Context;
 use std::path::PathBuf;
 use tree_sitter::{Parser, Tree, TreeCursor};
 
 pub(crate) struct ProcessedFile {
     pub(super) tree: Tree,
     pub(super) path: PathBuf,
-    pub(super) src: String,
 }
 impl ProcessedFile {
     pub(crate) fn new(parser: &mut Parser, path: PathBuf) -> ProcessedFile {
         let src: String = String::from_utf8(std::fs::read(&path).expect("Failed to read file."))
             .expect("Source code must be UTF8 encoded");
         let tree = parser.parse(&src, None).unwrap();
-        ProcessedFile { tree, path, src }
+        ProcessedFile { tree, path }
     }
     pub(crate) fn tree(&self) -> &Tree {
         &self.tree
@@ -28,7 +28,11 @@ impl ProcessedFile {
             .find(|x| range <= *x.range())
     }
     pub(crate) fn class(&self) -> anyhow::Result<Class> {
-        let mut class = Class::extract_from(&mut self.tree.walk(), self.src.as_str())?;
+        let src: String =
+            String::from_utf8(std::fs::read(&self.path).context("Failed to read file.")?)
+                .context("Source code must be UTF8 encoded")?;
+        let mut class =
+            Class::extract_from(&mut self.tree.walk(), src.as_str()).context("Parsing of class")?;
         class.add_location(&self.path);
         Ok(class)
     }
