f701de9f9eab03cd5d8a28015ea68a2733d2f2e4
Alessandro Schena <Alessandro.Schena@constructor.org>
2024-10-07 13:16:08 +0200

    feat: Allow parsing of libraries from config file (ecf)

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -1,22 +1,27 @@
 use serde::Deserialize;
-#[derive(Deserialize, Debug, PartialEq)]
+#[derive(Deserialize, Debug, PartialEq, Clone)]
 struct Config {
     system: System,
 }
-#[derive(Deserialize, Debug, PartialEq)]
+#[derive(Deserialize, Debug, PartialEq, Clone)]
 struct System {
     target: Target,
 }
-#[derive(Deserialize, Debug, PartialEq)]
+#[derive(Deserialize, Debug, PartialEq, Clone)]
 struct Target {
     cluster: Vec<Cluster>,
+    library: Option<Vec<Library>>,
 }
-
-#[derive(Deserialize, Debug, PartialEq)]
+#[derive(Deserialize, Debug, PartialEq, Clone)]
 struct Cluster {
     name: String,
     location: String,
     recursive: bool,
+}
+#[derive(Deserialize, Debug, PartialEq, Clone)]
+struct Library {
+    name: String,
+    location: String,
 }
 #[cfg(test)]
 mod tests {
@@ -43,6 +48,30 @@ mod tests {
 			location="./levenshtein_distance/" recursive="true" />
 	</target>
 </system>"#;
+    const XML_EXAMPLE_LIBRARY: &str = r#"<?xml version="1.0" encoding="ISO-8859-1"?>
+<system xmlns="http://www.eiffel.com/developers/xml/configuration-1-16-0"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://www.eiffel.com/developers/xml/configuration-1-16-0 http://www.eiffel.com/developers/xml/configuration-1-16-0.xsd"
+	name="sanity-check" uuid="6BE01FDA-BFC4-43D8-9182-99C7A5EFA7E9">
+	<target name="sanity-check">
+		<root all_classes="true" />
+		<file_rule>
+			<exclude>/\.git$</exclude>
+			<exclude>/\.svn$</exclude>
+			<exclude>/CVS$</exclude>
+			<exclude>/EIFGENs$</exclude>
+		</file_rule>
+		<capability>
+			<void_safety support="all" />
+		</capability>
+		<library name="base" location="$AP/library/base/base-scoop-safe.ecf" />
+		<cluster name="list_inversion"
+			location="./list_inversion/" recursive="true" />
+		<cluster name="levenshtein_distance"
+			location="./levenshtein_distance/" recursive="true" />
+	</target>
+</system>
+"#;
     #[test]
     fn extract_cluster() {
         let system: System = serde_xml_rs::from_str(XML_EXAMPLE).unwrap();
@@ -51,5 +80,17 @@ mod tests {
         assert_eq!(cluster.name, "list_inversion".to_string());
         assert_eq!(cluster.location, "./list_inversion/".to_string());
         assert!(cluster.recursive);
+    }
+    #[test]
+    fn extract_library() {
+        let system: System = serde_xml_rs::from_str(XML_EXAMPLE_LIBRARY).unwrap();
+        let target = system.target;
+        let libraries = target.library.expect("Library is present");
+        let library = libraries.first().expect("At least a library");
+        assert_eq!(library.name, "base".to_string());
+        assert_eq!(
+            library.location,
+            "$AP/library/base/base-scoop-safe.ecf".to_string()
+        );
     }
 }
