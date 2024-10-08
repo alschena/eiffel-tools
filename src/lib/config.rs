83b2907aebb078f620d6db8cbb6019ee89b01705
Alessandro Schena <Alessandro.Schena@constructor.org>
2024-10-08 20:14:22 +0200

    fix: add tests for extracting clusters from library and expanding shell variables in strings.

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -123,7 +123,7 @@ mod tests {
 			location="./levenshtein_distance/" recursive="true" />
 	</target>
 </system>"#;
-    const XML_EXAMPLE_LIBRARY: &str = r#"<?xml version="1.0" encoding="ISO-8859-1"?>
+    const XML_LIBRARY_CONFIG: &str = r#"<?xml version="1.0" encoding="ISO-8859-1"?>
 <system xmlns="http://www.eiffel.com/developers/xml/configuration-1-16-0"
 	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 	xsi:schemaLocation="http://www.eiffel.com/developers/xml/configuration-1-16-0 http://www.eiffel.com/developers/xml/configuration-1-16-0.xsd"
@@ -139,7 +139,27 @@ mod tests {
 		<capability>
 			<void_safety support="all" />
 		</capability>
-		<library name="base" location="$AP/library/base/base-scoop-safe.ecf" />
+		<cluster name="lib"
+			location="./lib/" recursive="true" />
+	</target>
+</system>"#;
+    const XML_EXAMPLE_WITH_LIBRARY: &str = r#"<?xml version="1.0" encoding="ISO-8859-1"?>
+<system xmlns="http://www.eiffel.com/developers/xml/configuration-1-16-0"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://www.eiffel.com/developers/xml/configuration-1-16-0 http://www.eiffel.com/developers/xml/configuration-1-16-0.xsd"
+	name="sanity-check" uuid="6BE01FDA-BFC4-43D8-9182-99C7A5EFA7E9">
+	<target name="sanity-check">
+		<root all_classes="true" />
+		<file_rule>
+			<exclude>/\.git$</exclude>
+			<exclude>/\.svn$</exclude>
+			<exclude>/CVS$</exclude>
+			<exclude>/EIFGENs$</exclude>
+		</file_rule>
+		<capability>
+			<void_safety support="all" />
+		</capability>
+		<library name="base" location="$AP/library_config.ecf" />
 		<cluster name="list_inversion"
 			location="./list_inversion/" recursive="true" />
 		<cluster name="levenshtein_distance"
@@ -158,24 +178,64 @@ mod tests {
     }
     #[test]
     fn extract_library() {
-        let system: System = serde_xml_rs::from_str(XML_EXAMPLE_LIBRARY).unwrap();
+        let system: System = serde_xml_rs::from_str(XML_EXAMPLE_WITH_LIBRARY).unwrap();
         let target = system.target;
         let libraries = target.library.expect("Library is present");
         let library = libraries.first().expect("At least a library");
         assert_eq!(library.name, "base".to_string());
-        assert_eq!(
-            library.location,
-            "$AP/library/base/base-scoop-safe.ecf".to_string()
-        );
+        assert_eq!(library.location, "$AP/library_config.ecf".to_string());
     }
     #[test]
-    #[ignore]
-    fn extract_clusters_in_library() {
-        let system: System = serde_xml_rs::from_str(XML_EXAMPLE_LIBRARY).unwrap();
-        let target = system.target;
-        let libraries = target.library.expect("Library is present");
-        let library = libraries.first().expect("At least a library");
-        assert_eq!(library.name, "base".to_string());
+    fn expand_path_containing_environment_variables() {
+        let path_with_environment_variables = "$AP/library_config.ecf".to_string();
+        std::env::set_var("AP", std::env::temp_dir());
+        let expanded_path = shellexpand::env(&path_with_environment_variables)
+            .expect("Expansion of library location");
+        let path = std::env::temp_dir().join(std::path::PathBuf::from("library_config.ecf"));
+        assert_eq!(
+            expanded_path,
+            path.to_str()
+                .expect("Path to string (might fail on windows)")
+        )
+    }
+    #[test]
+    fn all_clusters() -> anyhow::Result<()> {
+        let system: System = serde_xml_rs::from_str(XML_EXAMPLE_WITH_LIBRARY)
+            .expect("Parsable {XML_EXAMPLE_LIBRARY}");
+        match system.target.library.clone() {
+            Some(lib) => {
+                for loc in lib.iter() {
+                    let file = NamedTempFile::new(
+                        shellexpand::env(loc.location.as_str())
+                            .expect("Expand library location into valid path")
+                            .as_ref(),
+                    )
+                    .context("Create named temp file")?;
+                    file.write_str(XML_LIBRARY_CONFIG)
+                        .expect("Write to temp file");
+                }
+            }
+            None => panic!("Parsable library"),
+        }
+        assert!(system
+            .clone()
+            .clusters()
+            .context("All clusters location")?
+            .contains(&Cluster {
+                location: "./lib/".to_string(),
+                name: "lib".to_string(),
+                recursive: true,
+            }));
+        assert!(system
+            .clusters()
+            .context("All clusters location")?
+            .contains(&Cluster {
+                name: "levenshtein_distance".to_string(),
+                location: "./levenshtein_distance/".to_string(),
+                recursive: true
+            }));
+        Ok(())
+    }
         assert_eq!(
             library.location,
             "$AP/library/base/base-scoop-safe.ecf".to_string()
