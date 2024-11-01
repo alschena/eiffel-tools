5de9e3eac1cdd1a6df086dc8a0b1cb433b939033
Ilgiz Mustafin <ilgimustafin@gmail.com>
2024-11-01 17:55:31 +0100

    feat: allow ecf_path in Initialize

--- src/lib/language_server_protocol/requests/initialize.rs
+++ src/lib/language_server_protocol/requests/initialize.rs
@@ -5,13 +5,26 @@ use async_lsp::lsp_types::{
 };
 use async_lsp::ResponseError;
 use std::future::Future;
+use std::path::PathBuf;
 
 impl HandleRequest for Initialize {
     fn handle_request(
-        _st: ServerState,
+        st: ServerState,
         params: <Self as Request>::Params,
     ) -> impl Future<Output = Result<<Self as Request>::Result, ResponseError>> + Send + 'static
     {
+        params.initialization_options.map(|x| {
+            x.as_object().map(|z| {
+                z.get("ecf_path").map(|path| {
+                    let mut workspace =
+                        st.workspace.write().expect("workspace must be writable");
+                    workspace.set_ecf_path(Some(
+                        PathBuf::try_from(path.as_str().expect("must be string"))
+                            .expect("must be a valid path"),
+                    ));
+                });
+            })
+        });
         async move {
             Ok(InitializeResult {
                 capabilities: ServerCapabilities {
