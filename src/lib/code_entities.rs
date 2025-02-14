bf5fa425fedbed0a26416952991775c24454c6b1
Alessandro Schena <alessandro.schena@constructor.org>
2025-02-14 10:30:31 +0100

    refactor: prompt

--- src/lib/code_entities.rs
+++ src/lib/code_entities.rs
@@ -3,11 +3,12 @@ pub(crate) mod contract;
 mod feature;
 mod shared;
 pub(crate) mod prelude {
-    pub(crate) use super::class::model::Model as ClassModel;
+    pub(crate) use super::class::model::ModelExtended as ClassModel;
     pub(crate) use super::class::Class;
     pub(crate) use super::contract;
     pub(crate) use super::feature::EiffelType;
     pub(crate) use super::feature::Feature;
+    pub(crate) use super::feature::Parameters as FeatureParameters;
     pub(crate) use super::shared::{Location, Point, Range};
     pub(crate) use super::Indent;
 }
