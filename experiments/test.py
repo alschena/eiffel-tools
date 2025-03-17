7ca4caecf796b150bcda6a2fabd40138969c39a4
Alessandro Schena <alessandro.schena@constructor.org>
2025-03-17 19:34:24 +0100

    feat: collect output examples
    refactor: test script
    feat: add a couple of outputs

--- experiments/test.py
+++ experiments/test.py
@@ -2,81 +2,92 @@ import requests
 import os
 import json
 
-TOKEN = os.getenv('CONSTRUCTOR_APP_API_TOKEN')
-END_POINT='https://training.constructor.app/api/platform-kmapi/v1'
-HEADERS = {
-    'X-KM-AccessKey': f'Bearer {TOKEN}'
-}
-
-def any_knowledge_model_id():
-    response = requests.get(f'{END_POINT}/knowledge-models', headers=HEADERS).json()
-    id = response ['results'][0]['id']
-    print(f"ID:\t{id}")
-    return id
-
-
-SYSTEM_MESSAGE={
-    "role": "system",
-    "content": """You are a coding assistant, expert in the Eiffel programming language and in formal methods.
+class Model:
+	default_content_system_message = """You are a coding assistant, expert in the Eiffel programming language and in formal methods.
 You have extensive training in the usage of AutoProof, the static verifier of Eiffel.
-You will receive a prompt in eiffel code with holes of the form <ADD_*>.
 Write only model-based contracts, i.e. all qualified calls in all contract clauses will refer to the model of the target class and all unqualified calls in all contract clauses will refer to the model of the current class or its ancestors.
-Respond with the same code, substituting the holes with valid eiffel code.
-""",
-    "name": "Coding assistant"}
+Respond with the same code, substituting the holes with valid eiffel code. """
 
-SYSTEM_MESSAGE={
-    "role": "system",
-    "content": """You are a coding assistant, expert in the Eiffel programming language and in formal methods.
-You have extensive training in the usage of AutoProof, the static verifier of Eiffel.
-You will receive a prompt in eiffel code with holes of the form <ADD_*>.
-Write only model-based contracts, i.e. all qualified calls in all contract clauses will refer to the model of the target class and all unqualified calls in all contract clauses will refer to the model of the current class or its ancestors.
-Respond with the same code, substituting the holes with valid eiffel code.
-""",
-    "name": "Coding assistant"}
+	default_content_user_message = """note
+	model: model_feature1, model_feature2
+class
+	MODEL_BASED_CONTRACTS
 
-user_message={"role": "user", "content":
-    """-- For the current class and its ancestors, the model is value: INTEGER
--- the model is implemented in Boogie.
--- For the argument other: NEW_INTEGER
---  the model is value: INTEGER
---    the model is terminal, no qualified call on it is allowed.
-smaller (other: NEW_INTEGER): BOOLEAN
-	do
-		Result := value < other.value
-	ensure
-		Result = (value < other.value)
-	end
-"""}
+feature
 
-user_message={"role": "user", "content":
-    """-- For the current class and its ancestors, its model is: m: INTEGER
--- 	its model is terminal, no qualified call is allowed on this value.
--- For the argument arg: INTEGER
--- 	its model is terminal, no qualified call is allowed on this value.
-max_arg_m(arg: INTEGER): INTEGER
-	require
-			<ADD_PRECONDITION_CLAUSES>
-	do
-			if m < arg then 
-				Result := arg
-			else
-				Result := m
-			end
-	ensure
-			<ADD_POSTCONDITION_CLAUSES>
-	end
-"""}
+	model_feature1: INTEGER
+	model_feature2: INTEGER
+	not_model_feature1: INTEGER
+	not_model_feature2: INTEGER
 
-messages = [SYSTEM_MESSAGE, user_message]
+	-- Add model-based contracts to the following feature, responding only in eiffel code.
+	min: INTEGER
+		require
+			model_is_synced1: model_feature1 = not_model_feature1
+			model_is_synced2: model_feature2 = not_model_feature2
+		do
+			if not_model_feature1 < not_model_feature2 then
+				Result := not_model_feature1
+			else 
+				Result := not_model_feature2
+		end
+end """
+	TOKEN = os.getenv('CONSTRUCTOR_APP_API_TOKEN')
+	END_POINT='https://training.constructor.app/api/platform-kmapi/v1'
+	HEADERS = {
+	    'X-KM-AccessKey': f'Bearer {TOKEN}'
+	}
 
-def send_message_with_context(model, messages, stream="false"):
-    data = {"model": model, "messages": messages, "stream":stream}
-    response = requests.post(f'{END_POINT}/knowledge-models/{any_knowledge_model_id()}/chat/completions', headers=HEADERS, json=data)
-    return response.json()
+	def _any_knowledge_model_id(self):
+	    response = requests.get(f'{self.END_POINT}/knowledge-models', headers=self.HEADERS).json()
+	    id = response ['results'][0]['id']
+	    print(f"ID:\t{id}")
+	    return id
 
-response = send_message_with_context("gemini-1.5-flash", messages)
+	def _message(self, role, content):
+		res = {
+			'role': role,
+			'content': content
+		}
+		return res
 
+	def _named_message(self, role, content, name):
+		message = self._message(role, content)
+		message['name'] = name
+		return message
+
+	def _system_message(self, content):
+		return self._named_message("system", content, "Coding assistant")
+
+	def _user_message(self, content):
+		return self._message("user", content)
+
+	def __init__(self):
+		self.km_id = self._any_knowledge_model_id()
+		url = f'{self.END_POINT}/alive'
+		alive_response = requests.post(url, headers = self.HEADERS)
+		print(f'{alive_response}') 
+
+	def messages(self,
+	               system_message_content = default_content_system_message,
+	               user_message_content = default_content_user_message):
+		system_message = self._system_message(system_message_content)
+		user_message = self._user_message(user_message_content)
+		print(f'system_message: {system_message}')
+		return [system_message, user_message]
+
+	def send_messages(self, messages, model = "gemini-1.5-pro", stream="false"):
+	    data = {"model": model, "messages": messages, "stream":stream}
+	    print(f'data: {data}')
+	    url = f'{self.END_POINT}/knowledge-models/{self.km_id}/chat/completions'
+	    print(f'url: {url}')
+	    response = requests.post(url, headers=self.HEADERS, json=data)
+	    print(f'response: {response}')
+	    return response.json()
+
+model = Model()
+messages = model.messages()
+response = model.send_messages(messages)
 val = json.dumps(response)
 
 print(f'val: {val}')
